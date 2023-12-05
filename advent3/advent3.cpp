c8a8b057c4f455695621722846bd88c314d01c3a
Loïc Musy <loic@famille-musy.com>
2023-12-05 16:16:47 +0100

    Finishing Day 3 Part 1

--- advent3/advent3.cpp
+++ advent3/advent3.cpp
@@ -22,93 +22,68 @@ set<char> findSymbols(vector<string> mat){
 
 }
 
-int findDigitR(vector<string> mat, int line_idx, int char_idx){
-    string digit = "";
-
-    for (int i = char_idx + 1; i < mat[line_idx].size() && isdigit(mat[line_idx][i]); i++){
-        digit += mat[line_idx][i];
-    }
-
-    if(digit != "")
-        return stoi(digit);
-    else
-        return 0;
-}
-
-int findDigitL(vector<string> mat, int line_idx, int char_idx){
-    string digit = "";
-
-    for (int i = char_idx - 1; i >= 0 && isdigit(mat[line_idx][i]); i--){
-        digit = mat[line_idx][i] + digit;
-    }
-
-    if(digit != "")
-        return stoi(digit);
-    else
-        return 0;
-}
-
-int findDigitUD(vector<string> mat, int line_idx, int char_idx){
-    string digitstr = "";
-    int digit = 0;
-
-    if (line_idx < 0 || line_idx > mat.size())// Check for out-of-bound.
-        return 0;
-
-
-    // Find if there is a number directly above/below
-    if (isdigit(mat[line_idx][char_idx])) {
-        for (int i = char_idx; i < mat[line_idx].size() && isdigit(mat[line_idx][i]); i++) {
-            digitstr += mat[line_idx][i];
-        }
-        for (int i = char_idx - 1; i > 0 && isdigit(mat[line_idx][i]); i--) {
-            digitstr = mat[line_idx][i] + digitstr;
-        }
-    }
-
-    if (digitstr != ""){ // If we found a number above/below, then there cannot be a number diagonally (since the digit would be part of the number directly above/below)
-        return stoi(digitstr);
-    }
-
-    // Check if there is number diagonally
-    else{
-        for (int i = char_idx + 1; i < mat[line_idx].size() && isdigit(mat[line_idx][i]); i++) {
-            digitstr += mat[line_idx][i];
-        }
-        if (digitstr != ""){
-            digit += stoi(digitstr);
-            digitstr = "";
-        }
-        for (int i = char_idx - 1; i > 0 && isdigit(mat[line_idx][i]); i--) {
-            digitstr = mat[line_idx][i] + digitstr;
-        }
-        if (digitstr != ""){
-            digit += stoi(digitstr);
-        }
-
-        return digit;
-    }
-
-}
-
-int findNeighbouringNumbers(vector<string> mat, int line_idx, int char_idx){
-    int sum = 0;
-
-    // Find if there is a number to the EAST of the symbol
-    sum += findDigitR(mat, line_idx, char_idx);
-    sum += findDigitL(mat, line_idx, char_idx);
-    sum += findDigitUD(mat, line_idx + 1, char_idx);
-    sum += findDigitUD(mat, line_idx - 1, char_idx);
+bool checkSymbol(vector<string> mat, int line_idx, int char_idx, set<char> symbols){
     
+        for(int line_shift : set<int> {0,1,-1}){
+            for(int char_shift: set<int> {0,1,-1}){
+
+                // Check for out of bound condition
+                bool c1 = ((line_idx + line_shift) >= mat.size());
+                bool c2 = ((line_idx + line_shift) < 0);
+                bool c3 = ((char_idx + char_shift) >= mat[line_idx].size());
+                bool c4 = ((char_idx + char_shift) < 0);
+                bool c = (c1 || c2 || c3 || c4);
+
+                if(c)
+                    continue;
+                else{
+                    for (char symbol : symbols){
+                        if(mat[line_idx + line_shift][char_idx + char_shift] == symbol)
+                            return true;
+                    }
+                }
+            }
+        }
+
+    return false;
+
+}
+
+int findValidNumber(vector<string> mat, set<char> symbols){
+    bool nextToSymbol = false;
+    char c;
+    string digits = "";
+    int sum = 0;
+    for (int line_idx=0; line_idx < mat.size(); line_idx++){
+        for(int char_idx=0; char_idx < mat[line_idx].size(); char_idx ++){
+            c = mat[line_idx][char_idx];
+            if(isdigit(c)){
+                digits += c;
+                if(!nextToSymbol){
+                    nextToSymbol = checkSymbol(mat, line_idx, char_idx, symbols);
+                }
+            }
+            else{
+                if(nextToSymbol && digits != ""){
+                    sum += stoi(digits);
+                }
+                digits = "";
+                nextToSymbol = false;
+            }
+        }
+            if(nextToSymbol && digits != ""){
+                sum += stoi(digits);
+            }
+            digits = "";
+            nextToSymbol = false;
+    }
+
     return sum;
 }
-
-
 
 int main(){
 
     vector<string> matrixText;
-    vector<vector<bool>> boolMatrix;
     set<char> symbols;
     string line;
     int sum = 0;
@@ -123,18 +98,7 @@ int main(){
 
     symbols = findSymbols(matrixText);
 
-    vector<bool> boolLine(matrixText[0].size(), false);
-    for (string line : matrixText){
-        boolMatrix.push_back(boolLine);
-    }
-
-    for (int line_idx = 0; line_idx < matrixText.size(); line_idx++){
-        for (int char_idx = 0; char_idx < matrixText[line_idx].size(); char_idx++){
-            if(symbols.find(matrixText[line_idx][char_idx]) != symbols.end()){
-                sum += findNeighbouringNumbers(matrixText, line_idx, char_idx);
-            }
-        }
-    }
+    sum = findValidNumber(matrixText, symbols);
 
     cout << sum;
 }
